5f54c10eae9401d4069eb4a81af6f40356336ffa
Robby De Pauw <robby.depauw@sciensano.be>
2021-08-12 17:36:04 +0200

    Add statbel DATA

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' STATBEL POPULATION STRUCTURE 2012-2021
+#'
+#' A dataset containing the population structure from 2012 to 2021
+#' based on the national registry.
+#'
+#' @format A data frame with 2222 rows and 4 variables:
+#' \describe{
+#'   \item{SEX}{SEX - Male of Female}
+#'   \item{AGE}{AGE of the population}
+#'   \item{YEAR}{YEAR on which data was based}
+#'   \item{POP}{NUMBER of inhabitants}
+#'   ...
+#' }
+#' @source \url{https://www.statbel.be/}
+"statbel"
